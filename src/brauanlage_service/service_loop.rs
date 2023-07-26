ee7b3316e040b9fcaa36befa16c4cdef445e21f7
Liam Friedrich <gh@jehov4.dev>
2023-07-26 23:11:32 +0200

    some more

--- src/brauanlage_service/service_loop.rs
+++ src/brauanlage_service/service_loop.rs
@@ -1,9 +1,10 @@
 use tokio::sync::{mpsc::Receiver, Mutex};
 
-use tokio::sync::watch::Sender;
+use tokio::sync::watch::{Sender, self};
+use tokio::sync::watch::Receiver as WReceiver;
 use tonic::Status;
 
-use super::{brauanlage::{TempStatus, RelayStatus, Rcp}, service::BrauCommand};
+use super::{brauanlage::{TempStatus, RelayStatus, Rcp, RcpStatus}, service::BrauCommand};
 
 
 struct ServiceLoop {
@@ -11,7 +12,6 @@ struct ServiceLoop {
     relay_sender: Mutex<Sender<RelayStatus>>,
     rcp_sender: Mutex<Sender<Rcp>>,
     command_receiver: Mutex<Receiver<BrauCommand>>,
-
 }
 
 impl ServiceLoop {
@@ -29,17 +29,54 @@ impl ServiceLoop {
         }
     }
     pub async fn service_loop(&self) {
+        let mut rcp_status = Rcp {
+            steps: Vec::new(),
+            status: RcpStatus::Uninitialized.into(),
+            step_started_timestamp: 0,
+        };
+
+        // channels for status communication to frontent
         let temps_sender = self.temps_sender.lock().await;
         let rcp_sender = self.rcp_sender.lock().await;
         let relay_sender = self.relay_sender.lock().await;
+
+        // channel for control commands from frontend
         let mut command_receiver = self.command_receiver.lock().await;
 
-        let command = command_receiver.try_recv();
-        if command.is_ok() {
-            unimplemented!()
+        // internal channel for relay control state irc
+        let (irc_sender, irc_receiver) = watch::channel(RelayStatus {stati: vec!(false,false)});
+        // internal channel for temperature control state itc
+        let (itc_sender, itc_receiver) = watch::channel(TempStatus {temps: vec!(0,0,0)});
+
+        // start control loops
+        tokio::task::spawn(Self::temp_loop(itc_receiver));
+        tokio::task::spawn(Self::relay_loop(irc_receiver));
+
+        loop {
+            let command = command_receiver.try_recv();
+            if command.is_ok() {
+                let foo = match command.unwrap() {
+                    BrauCommand::UpdateTemp(status) => itc_sender.send(status).is_ok(),
+                    BrauCommand::UpdateRelays(status) => irc_sender.send(status).is_ok(),
+                    BrauCommand::Next => unimplemented!(),
+                    BrauCommand::Start => unimplemented!(),
+                    BrauCommand::Pause => unimplemented!(),
+                    BrauCommand::TakeRcp(rcp) => {
+                        rcp_status.steps.clone_from(&rcp.steps);
+                        true
+                    }
+                };
+
+            }
         }
     }
     fn get_temps() -> TempStatus {
+        unimplemented!()
+    }
+    async fn temp_loop(control: WReceiver<TempStatus>){
+        unimplemented!()
+    }
+    async fn relay_loop(control: WReceiver<RelayStatus>){
         unimplemented!()
     }
 
