bd8056e97a270d96b050050831576f05c886ca35
Liam Friedrich <gh@jehov4.dev>
2023-08-02 01:59:23 +0200

    some elaborate fuckery

--- src/brauanlage_service.rs
+++ src/brauanlage_service.rs
@@ -1,5 +1,6 @@
 pub mod service;
 pub mod service_loop;
+pub mod peripheral;
 
 pub mod brauanlage {
     include!("brauanlage.rs");
--- src/brauanlage_service/service_loop.rs
+++ src/brauanlage_service/service_loop.rs
@@ -1,14 +1,18 @@
 use tokio::sync::{mpsc::Receiver, Mutex};
 
+use std::sync::Arc;
+
 use tokio::sync::watch::{Sender, self};
 use tokio::sync::watch::Receiver as WReceiver;
 use tonic::Status;
 
-use super::{brauanlage::{TempStatus, RelayStatus, Rcp, RcpStatus}, service::BrauCommand};
+use itertools::izip;
 
+use super::{brauanlage::{TempStatus, RelayStatus, Rcp, RcpStatus}, service::BrauCommand};
+use super::peripheral::Peripheral;
 
 struct ServiceLoop {
-    temps_sender: Mutex<Sender<Result<TempStatus, Status>>>,
+    temps_sender: Arc<Mutex<Sender<Result<TempStatus, Status>>>>,
     relay_sender: Mutex<Sender<RelayStatus>>,
     rcp_sender: Mutex<Sender<Rcp>>,
     command_receiver: Mutex<Receiver<BrauCommand>>,
@@ -16,7 +20,7 @@ struct ServiceLoop {
 
 impl ServiceLoop {
     pub async fn new(
-            temps_sender: Mutex<Sender<Result<TempStatus, Status>>>,
+            temps_sender: Arc<Mutex<Sender<Result<TempStatus, Status>>>>,
             relay_sender: Mutex<Sender<RelayStatus>>,
             rcp_sender: Mutex<Sender<Rcp>>,
             command_receiver: Mutex<Receiver<BrauCommand>>,
@@ -35,11 +39,6 @@ impl ServiceLoop {
             step_started_timestamp: 0,
         };
 
-        // channels for status communication to frontent
-        let temps_sender = self.temps_sender.lock().await;
-        let rcp_sender = self.rcp_sender.lock().await;
-        let relay_sender = self.relay_sender.lock().await;
-
         // channel for control commands from frontend
         let mut command_receiver = self.command_receiver.lock().await;
 
@@ -48,8 +47,10 @@ impl ServiceLoop {
         // internal channel for temperature control state itc
         let (itc_sender, itc_receiver) = watch::channel(TempStatus {temps: vec!(0,0,0)});
 
+        let temps_sender = self.temps_sender.clone();
+
         // start control loops
-        tokio::task::spawn(Self::temp_loop(itc_receiver));
+        tokio::task::spawn(Self::temp_loop(itc_receiver, temps_sender));
         tokio::task::spawn(Self::relay_loop(irc_receiver));
 
         loop {
@@ -73,11 +74,44 @@ impl ServiceLoop {
     fn get_temps() -> TempStatus {
         unimplemented!()
     }
-    async fn temp_loop(control: WReceiver<TempStatus>){
-        unimplemented!()
+
+    fn calc_switch_operations (goal: &TempStatus, current: &TempStatus) -> Vec<bool> {
+        let heating_buffer = 1;
+        let cooling_buffer = 1;
+        let mut operations = Vec::new();
+        for (goalv, currentv) in izip!(&goal.temps, &current.temps) {
+            if *currentv > *goalv + heating_buffer {
+                operations.push(false)
+            }
+            if *currentv < *goalv - cooling_buffer {
+                operations.push(true)
+            }
+        }
+        operations
     }
-    async fn relay_loop(control: WReceiver<RelayStatus>){
-        unimplemented!()
+
+    async fn temp_loop(mut control: WReceiver<TempStatus>, sender: Arc<Mutex<Sender<Result<TempStatus, Status>>>>){
+        let mut goal = control.borrow().clone();
+        let inner_sender = sender.lock().await;
+        loop {
+           let current = Self::get_temps();
+           Peripheral::set_relays(vec!(1,2,3), Self::calc_switch_operations(&goal, &current));
+           inner_sender.send(Ok(current));
+           if control.has_changed().unwrap() {
+              goal = control.borrow_and_update().clone(); 
+           }
+        }
+        
+    }
+    
+    async fn relay_loop(mut control: WReceiver<RelayStatus>){
+        loop {
+            Peripheral::set_relays(vec!(1,2), control.borrow().stati.clone());
+            let fut = control.changed().await; 
+            if fut.is_err()  {
+                println!("Something went wrong waiting for relay Updates")
+            };
+        }
     }
 
 }
